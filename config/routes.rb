a08943644513535adc7bc05cd0f70af731cd7fd8
Me Me Mon <memethetmon@yahoo.com>
2016-03-30 13:00:51 -0700

    installed haml-rails gem

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'video#index'
 
+  get '/room' => 'video#room'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
